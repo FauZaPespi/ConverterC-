f5997b2755d15c7a6fa89ec51e65b03aae4eac97
FauZa <160544435+FauZaPespi@users.noreply.github.com>
2024-05-11 02:02:29 +0200

    Bah l'ajout des fichier t con mec ?
    tu crois je me fait chier la bah oui jme chie dessu la

---
+++ ImageConverter/ImageConverter/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ImageConverter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxFormat = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblPalms = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(121, 60);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(139, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBoxFormat
+            // 
+            this.comboBoxFormat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxFormat.FormattingEnabled = true;
+            this.comboBoxFormat.Location = new System.Drawing.Point(401, 12);
+            this.comboBoxFormat.Name = "comboBoxFormat";
+            this.comboBoxFormat.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFormat.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(401, 39);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(121, 63);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblPalms
+            // 
+            this.lblPalms.AutoSize = true;
+            this.lblPalms.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblPalms.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPalms.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lblPalms.Location = new System.Drawing.Point(487, 270);
+            this.lblPalms.Name = "lblPalms";
+            this.lblPalms.Size = new System.Drawing.Size(35, 14);
+            this.lblPalms.TabIndex = 4;
+            this.lblPalms.Text = "Palms";
+            this.lblPalms.Click += new System.EventHandler(this.lblPalms_Click);
+            this.lblPalms.MouseEnter += new System.EventHandler(this.lblPalms_MouseEnter);
+            this.lblPalms.MouseLeave += new System.EventHandler(this.lblPalms_MouseLeave);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 292);
+            this.Controls.Add(this.lblPalms);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.comboBoxFormat);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpenFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Convertisseur";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxFormat;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblPalms;
+    }
+}
+
---
+++ ImageConverter/ImageConverter/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageConverter
+{
+    /// <summary>
+    /// Auteur: FauZaPespi
+    /// Date: 11/05/2024
+    /// Website: https://discord.palms.lol
+    /// Fichier: Form1.cs
+    /// Objectif: Faire un programme pour convertir juste comme ca flm de tous ces mecs...
+    /// </summary>
+    public partial class Main : Form
+    {
+        /// <summary>
+        /// Methode de main t con ou quoi ?
+        /// </summary>
+        public Main()
+        {
+            InitializeComponent();
+            comboBoxFormat.Items.AddRange(new string[] { "ICO", "PNG", "JPG", "GIF" });
+            comboBoxFormat.SelectedIndex = 0;  
+        }
+        /// <summary>
+        /// Methode pour ouvrir le dialog etc ta crampter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files (*.bmp, *.jpg, *.jpeg, *.jpe, *.jfif, *.png, *.gif)|*.bmp;*.jpg;*.jpeg;*.jpe;*.jfif;*.png;*.gif|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select an Image";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+        /// <summary>
+        /// Pour convertir en ico parce que sinon ca fait de la merde vu que les .ico c compliquer comme un casse tete ce mec
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="filePath"></param>
+        /// <param name="size"></param>
+        private void ConvertToIcon(Image image, string filePath, int size)
+        {
+            using (Bitmap squareBitmap = new Bitmap(size, size))
+            using (Graphics g = Graphics.FromImage(squareBitmap))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(image, 0, 0, size, size);
+                using (Icon icon = Icon.FromHandle(squareBitmap.GetHicon()))
+                {
+                    using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                    {
+                        icon.Save(fs);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Methode pour le boutton qui converti ca mere !!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Please select an image first.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "output";
+
+            switch (comboBoxFormat.SelectedItem.ToString())
+            {
+                case "ICO":
+                    saveFileDialog.Filter = "Icon files (*.ico)|*.ico";
+                    break;
+                case "PNG":
+                    saveFileDialog.Filter = "PNG files (*.png)|*.png";
+                    break;
+                case "JPG":
+                    saveFileDialog.Filter = "JPEG files (*.jpg)|*.jpg";
+                    break;
+                case "GIF":
+                    saveFileDialog.Filter = "GIF files (*.gif)|*.gif";
+                    break;
+            }
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Save the image in the selected format
+                switch (comboBoxFormat.SelectedItem.ToString())
+                {
+                    case "ICO":
+                        ConvertToIcon(pictureBox1.Image, saveFileDialog.FileName, 256);
+                        break;
+                    case "PNG":
+                        pictureBox1.Image.Save(saveFileDialog.FileName, ImageFormat.Png);
+                        break;
+                    case "JPG":
+                        pictureBox1.Image.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                        break;
+                    case "GIF":
+                        pictureBox1.Image.Save(saveFileDialog.FileName, ImageFormat.Gif);
+                        break;
+                }
+                MessageBox.Show("Image saved successfully.");
+            }
+        }
+
+        private void lblPalms_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.palms.lol");
+        }
+
+        #region truc de merde c juste pour le link pour faire un effet de hover
+        /// <summary>
+        /// Truc pour quand ca entre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblPalms_MouseEnter(object sender, EventArgs e)
+        {
+            lblPalms.ForeColor = Color.SeaGreen;
+        }
+        /// <summary>
+        /// Truc pour quand ca sort fdp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lblPalms_MouseLeave(object sender, EventArgs e)
+        {
+            lblPalms.ForeColor = Color.LightSeaGreen;
+        }
+        #endregion
+    }
+}
+
+/// Si tu skid t'es gay
+
+
